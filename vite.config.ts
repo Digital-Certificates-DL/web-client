6bc732fc356754ebe83a3b33ddb8394194191290
mark.cherepovskyi <mark.cherepovskyi@gmail.com>
2023-05-23 15:49:05 +0300

    hot fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -75,6 +75,19 @@ export default defineConfig(({ command, mode }) => {
     },
     build: {
       target: 'esnext',
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id
+                .toString()
+                .split('node_modules/')[1]
+                .split('/')[0]
+                .toString()
+            }
+          },
+        },
+      },
     },
     optimizeDeps: {
       exclude: ['@syntect/wasm'],
